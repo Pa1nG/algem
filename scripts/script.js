2c742b99f326e93dabe9d3d6c9b778d6c6523561
Pa1nG <121500165+Pa1nG@users.noreply.github.com>
2024-12-28 09:04:17 +0300

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const cards = document.getElementById("start");
+const theme = document.getElementById("theme");
+cards.addEventListener('click', () => {
+    window.location.href = 'pages/cards.html';
+})
+theme.addEventListener('click', () => {
+    window.location.href = 'pages/indexw.html';
+})
